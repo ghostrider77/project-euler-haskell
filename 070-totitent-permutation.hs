f76eb1cfe0ee9efe458732f34f63932a68d4a106
Tamas Kurics <tamas.kurics@gmail.com>
2020-02-07 20:41:34 +0100

    add totient permutation problem
    Although it is easy to come to the idea that the solution is the product
    of two primes, traversing the list of primes in a double for-loop is just
    as expensive as calculating the totitent function values. Reducing the
    range of primes is feasible, but did not find any reasonable explanation,
    so instead of using any 'educated guess', I rather post this solution, since
    this is for sure correct.
    
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ 070-totitent-permutation.hs
@@ -0,0 +1,45 @@
+import Data.List (sort)
+import Data.Vector (Vector, (!), freeze, generate, thaw)
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import Control.Monad.ST
+import Control.Monad (forM_)
+
+
+calcPhiValuesUsingSieve :: Int -> Vector Int
+calcPhiValuesUsingSieve limit =
+    runST $ do
+        let size = limit + 1
+        stateVector <- thaw (generate size id)
+        updatePhi stateVector limit
+        freeze stateVector
+
+
+updatePhi :: (MV.MVector s Int) -> Int -> ST s ()
+updatePhi v limit =
+    forM_ [2..limit] $ \ix -> do
+        x <- MV.read v ix
+        if x == ix then do
+            let indices = takeWhile (<= limit) $ map (*x) [1..]
+            forM_ indices $ \jy -> do
+                MV.modify v (\y -> (y - (y `div` x))) jy
+        else return ()
+
+
+minimumTotientQuotient :: Int -> Int
+minimumTotientQuotient limit = fst $ foldl processNext (1, 2) [3,5..limit]
+    where
+        phiVector = calcPhiValuesUsingSieve limit
+        isPermutation :: Int -> Int -> Bool
+        isPermutation n m = (sort $ show m) == (sort $ show n)
+        processNext :: (Int, Double) -> Int -> (Int, Double)
+        processNext acc @ (minN, minRatio) n =
+            let phi = phiVector ! n
+                ratio = (fromIntegral n) / (fromIntegral phi)
+            in if ratio >= minRatio || not (isPermutation phi n) then acc else (n, ratio)
+
+
+main :: IO()
+main = do
+    let limit = 10000000
+    print $ minimumTotientQuotient limit
