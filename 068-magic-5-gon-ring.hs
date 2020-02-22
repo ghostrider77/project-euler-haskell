96f79a309fcb24dd1227f06e244bf487ee9d613d
Tamas Kurics <tamas.kurics@gmail.com>
2020-02-22 19:03:32 +0100

    add magic 5-gon ring problem
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

---
+++ 068-magic-5-gon-ring.hs
@@ -0,0 +1,35 @@
+import Data.List (permutations, sort)
+import Data.Vector (Vector, (!))
+import qualified Data.Vector as V
+
+
+
+lineIndices :: [[Int]]
+lineIndices = [[0, 1, 2], [3, 2, 4], [5, 4, 6], [7, 6, 8], [9, 8, 1]]
+
+
+
+stringRepresentation :: Vector Int -> String
+stringRepresentation permutation =
+    let doubleList = lineIndices ++ lineIndices
+        size = length lineIndices
+        cyclicalIndices = [take size $ drop k doubleList | k <- [0..(size-1)]]
+        intLists = [concat $ map (\ixs -> map (permutation !) ixs) indices | indices <- cyclicalIndices]
+    in concat $ map show $ head $ sort intLists
+
+
+maximum16DigitString :: [Int] -> String
+maximum16DigitString numbers =
+    let perms = map V.fromList $ permutations numbers
+        isValid :: Vector Int -> Bool
+        isValid permutation =
+            let lineSum = V.sum $ V.take 3 permutation
+            in all (\indices -> lineSum == (sum $ map (permutation !) indices)) lineIndices
+        validPerms = filter isValid perms
+    in maximum $ filter (\s -> length s == 16) $ map stringRepresentation validPerms
+
+
+main :: IO()
+main = do
+    let numbers = [1..10]
+    putStrLn $ maximum16DigitString numbers
