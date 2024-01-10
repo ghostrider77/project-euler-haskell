49940293e939a7abf45329c2beb5ddeea15169a7
Tamas Kurics <tamas.kurics@gmail.com>
2024-01-10 17:58:57 +0100

    fix indentation level
    Signed-off-by: Tamas Kurics <tamas.kurics@gmail.com>

--- 018-maximum-path-sum-1.hs
+++ 018-maximum-path-sum-1.hs
@@ -14,17 +14,17 @@ convertToMatrix rows n = listArray ((0, 0), (n - 1, n - 1)) $ concat $ map padwi
 
 calcMaxPathSum :: Array (Int, Int) Int -> Int -> Int
 calcMaxPathSum matrix n = maximum $ map (\colIx -> maxPathSumMatrix ! (n - 1, colIx)) [0..(n - 1)]
-        where
-            maxPathSum :: Int -> Int -> Int
-            maxPathSum 0 0 = matrix ! (0, 0)
-            maxPathSum i 0 = maxPathSumMatrix ! (i - 1, 0) + matrix ! (i, 0)
-            maxPathSum i j
-                | j > i = 0
-                | otherwise =
-                    let number = matrix ! (i, j)
-                    in max (maxPathSumMatrix ! (i - 1, j) + number) (maxPathSumMatrix ! (i - 1, j - 1) + number)
-            bounds = ((0, 0), (n - 1, n - 1))
-            maxPathSumMatrix = listArray bounds [maxPathSum ix jy | (ix, jy) <- range bounds]
+    where
+        maxPathSum :: Int -> Int -> Int
+        maxPathSum 0 0 = matrix ! (0, 0)
+        maxPathSum i 0 = maxPathSumMatrix ! (i - 1, 0) + matrix ! (i, 0)
+        maxPathSum i j
+            | j > i = 0
+            | otherwise =
+                let number = matrix ! (i, j)
+                in max (maxPathSumMatrix ! (i - 1, j) + number) (maxPathSumMatrix ! (i - 1, j - 1) + number)
+        bounds = ((0, 0), (n - 1, n - 1))
+        maxPathSumMatrix = listArray bounds [maxPathSum ix jy | (ix, jy) <- range bounds]
 
 
 main :: IO()
